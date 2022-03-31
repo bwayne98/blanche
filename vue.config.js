21856d065bae3fb693f8b460588ca5b11847e25d
bwayne98 <bm.stamina@gmail.com>
2022-03-31 22:28:29 +0800

    github page 設定

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,8 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,  //dev下格式不受限
+
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/blanche/' // test20200915 為 repo 名稱
+    : '/'
 });
